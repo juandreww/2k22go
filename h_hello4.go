038f208c88400e947af241caebd1cc96294819ce
juandreww <juandreww@outlook.com>
2022-07-30 20:59:08 +0700

    zxcv

--- h_hello4.go
+++ h_hello4.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello 4")
-	fmt.Println("Sample A")
+	fmt.Println("Sample A: Continue")
 	for i := 1; i < 20; i++ {
 		if i % 3 == 0 {
 			continue
@@ -12,5 +12,12 @@ func main() {
 		fmt.Println(i)
 	}
 
-	fmt.Println("Sample B")
+	fmt.Println("Sample B: Break")
+	for i := 1; i < 20; i++ {
+		if i % 3 == 0 {
+			fmt.Printf("Break at seq: %v\n", i)
+			break
+		}
+		fmt.Println(i)
+	}
 }
