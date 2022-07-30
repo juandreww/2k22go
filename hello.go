ab1913fa60e0e60119345abc18194859a696bc53
juandreww <juandreww@outlook.com>
2022-07-30 15:08:31 +0700

    jasper

--- hello.go
+++ hello.go
@@ -12,6 +12,6 @@ func task() {
 	arr := []int{1,2,3,4}
 	arr2 := []string{"mang", "tas", "net", "work"}
 	arr = append(arr, 1,2,3)
-	arr2 = append(arr2, "1","2","3")
+	arr2 = append(arr2, "1","2","3", "jasper")
 	fmt.Println(arr, arr2)
 }
