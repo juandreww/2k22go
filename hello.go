219ea687c35f29398577e238d9f153d835da3b95
juandreww <juandreww@outlook.com>
2022-07-30 15:44:42 +0700

    ccc

--- hello.go
+++ hello.go
@@ -4,16 +4,13 @@ import (
 	"fmt"
 )
 
-func swap(m1, m2 *int) {
-	var temp int
-	temp = *m2
-	*m2 = *m1
-	*m1 = temp
+type Car struct {
+	Name string
+	Age int
+	PhoneNo int
 }
 
 func main() {
-	m1, m2 := 2, 3
-	fmt.Println(m1, m2)
-	swap(&m1, &m2)
-	fmt.Println(m1, m2)
+	c := Car{"Adam", 24, 9899100}
+	fmt.Println(c)
 }
