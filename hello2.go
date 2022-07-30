452173aad744f03d4543bbc59bb4a6ec2710407d
juandreww <juandreww@outlook.com>
2022-07-30 19:45:37 +0700

    interface uncertainty

---
+++ hello2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func Anything(anything interface{}) {
+	fmt.Println(anything)
+}
+
+func main() {
+	fmt.Println("Hello 2")
+	Anything(2.55)
+	Anything("Jamal")
+	Anything(struct{} {})
+
+	myMap := make(map[string]interface{})
+	myMap["name"] = "Jamal Malu"
+	myMap["age"] = 15
+	fmt.Println(myMap)
+}
