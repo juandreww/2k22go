1a9138f461e5871b5f080481b1dc974ef112db39
juandreww <juandreww@outlook.com>
2022-07-30 15:13:03 +0700

    vvv

--- hello.go
+++ hello.go
@@ -5,13 +5,7 @@ import (
 )
 
 func main() {
-	task()
-}
-
-func task() {
-	arr := []int{1,2,3,4}
-	arr2 := []string{"mang", "tas", "net", "work"}
-	arr = append(arr, 1,2,3)
-	arr2 = append(arr2, "1","2","3", "jasper")
-	fmt.Println(arr, arr2)
+	m1 := 3
+	m2 := "yes"
+	fmt.Println(&m1, &m2)
 }
