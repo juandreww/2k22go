8536d4ef9224580772f91ca9dc911859c742ac66
juandreww <juandreww@outlook.com>
2022-07-30 15:08:17 +0700

    asdf

--- hello.go
+++ hello.go
@@ -5,14 +5,13 @@ import (
 )
 
 func main() {
-	// var arr []int
+	task()
+}
+
+func task() {
 	arr := []int{1,2,3,4}
 	arr2 := []string{"mang", "tas", "net", "work"}
 	arr = append(arr, 1,2,3)
 	arr2 = append(arr2, "1","2","3")
 	fmt.Println(arr, arr2)
-}
-
-func task() {
-
 }
