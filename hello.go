1d99f237754bb77ab157e0ce119d68ab9a8d659a
juandreww <juandreww@outlook.com>
2022-07-30 15:07:10 +0700

    zxcv

--- hello.go
+++ hello.go
@@ -7,8 +7,9 @@ import (
 func main() {
 	// var arr []int
 	arr := []int{1,2,3,4}
+	arr2 := []string{"mang", "tas", "net", "work"}
 	arr = append(arr, 1,2,3)
-	fmt.Println(arr)
+	fmt.Println(arr, arr2)
 }
 
 func task() {
