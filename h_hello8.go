f197a2a8641cb51fbf6b94e6731685f527e332e6
juandreww <juandreww@outlook.com>
2022-08-01 06:50:58 +0700

    hello 8

--- h_hello8.go
+++ h_hello8.go
@@ -1,28 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-type CarFree struct {
-	Model string
-}
+// type CarFree struct {
+// 	Model string
+// }
 
-func main() {
-	c := make(chan *CarFree, 3)
+// func main() {
+// 	c := make(chan *CarFree, 3)
 
-	go func() {
-		c <- &CarFree{"1"}
-		c <- &CarFree{"2"}
-		c <- &CarFree{"3"}
-		c <- &CarFree{"4"}
-		close(c)
-	}()
+// 	go func() {
+// 		c <- &CarFree{"1"}
+// 		c <- &CarFree{"2"}
+// 		c <- &CarFree{"3"}
+// 		c <- &CarFree{"4"}
+// 		close(c)
+// 	}()
 
-	// fmt.Println("Fin")
-	for i := range c {
-		fmt.Println(i.Model)
-	}
-	// fmt.Println("Done")
-}
+// 	// fmt.Println("Fin")
+// 	for i := range c {
+// 		fmt.Println(i.Model)
+// 	}
+// 	// fmt.Println("Done")
+// }
 
