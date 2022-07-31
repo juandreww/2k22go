b9967f1ccf4c1cf62815f140ac36abe32bfdbc9a
juandreww <juandreww@outlook.com>
2022-08-01 06:58:20 +0700

    asdf

--- h_hello9.go
+++ h_hello9.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func pinger()
+
+func ponger()
+
 func main() {
-	fmt.Println("LeetCode urgently")
+	ping := make(chan int)
+	pong := make(chan int)
+
+	go pinger(ping, pong)
+	go ponger(ping, pong)
+
+	ping <- 1
+
+	for {
+		time.Sleep(time.Second * 1)
+	}
 }
