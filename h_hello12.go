640920aeaa4b8a78183a1647e9fe73abcc398a1b
juandreww <juandreww@outlook.com>
2022-08-03 06:39:34 +0700

    status ok

--- h_hello12.go
+++ h_hello12.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"net/http"
-	"fmt"
+	"encoding/json"
+	// "fmt"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			data := structs.Response{
+				Code : http.StatusOK,
+				Body : "pong",
+			}
+			json.NewEncoder(w).Encode(data)
+		}
+	})
 	http.ListenAndServe(":80", nil)
 }
