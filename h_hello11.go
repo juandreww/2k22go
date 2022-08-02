6c8e0ea59c92b5382f35796a33f214aa598ffba0
juandreww <juandreww@outlook.com>
2022-08-03 06:17:43 +0700

    xx

--- h_hello11.go
+++ h_hello11.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/getgoing", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Request received");
+		fmt.Println(r.Method)
 		w.Write([]byte("Hello World"))
 	})
 	fmt.Println("listening")
