894b0708d3d99a79276d88d903a2126e51bdda1a
juandreww <juandreww@outlook.com>
2022-08-05 07:36:38 +0700

    jalapenozxcv

--- h_hello13.go
+++ h_hello13.go
@@ -9,16 +9,6 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			fmt.Println("Request received")
-			data := views.Response{
-				Code : http.StatusOK,
-				Body : "pong",
-			}
-			
-			json.NewEncoder(w).Encode(data)
-		}
-	})
+	
 	http.ListenAndServe(":80", mux)
 }
