64c20b70c87c8e9e1cceaaa2c59eb4ce71520676
juandreww <juandreww@outlook.com>
2022-08-09 07:07:17 +0700

    zxcv

--- model/connect.go
+++ model/connect.go
@@ -10,13 +10,16 @@ const (
 	host = "localhost"
 	port = 5432
 	user = "awtpbzyctlpydm"
-	pass = "a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96"
+	password = "a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96"
 	dbname = "d4ehughfapgq0k"
 
 )
 
 func Connect() *sql.DB {
-	db, err := sql.Open("postgres", "awtpbzyctlpydm:a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96@(tcp:localhost:5432)")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=disable",
+    host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
