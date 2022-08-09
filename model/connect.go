561945759eee06138c4faa17f72895057a738f8e
juandreww <juandreww@outlook.com>
2022-08-09 07:00:02 +0700

    postgres

--- model/connect.go
+++ model/connect.go
@@ -6,6 +6,12 @@ import (
 	"fmt"
 )
 
+const (
+	host = "localhost"
+	port = 5432
+	
+)
+
 func Connect() *sql.DB {
 	db, err := sql.Open("postgres", "awtpbzyctlpydm:a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96@(tcp:localhost:5432)")
 	if err != nil {
