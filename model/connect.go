8fa9f61cc7bb48cb09509f8aac2fddfed8aafefe
juandreww <juandreww@outlook.com>
2022-08-12 06:45:29 +0700

    xxx

--- model/connect.go
+++ model/connect.go
@@ -32,17 +32,7 @@ func Connect() *sql.DB {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// var result Result
-	// resultSql:= "SELECT uid, type2, quantity FROM trnkelapabakar WHERE uid='00f713dc-5649-4a0a-96c6-d442b81a0aa1'"
 	
-	// err = db.QueryRow(resultSql).Scan(&result.uid, &result.type2, &result.quantity)
-
-	// if err != nil {
-	// 	log.Fatal("Failed to execute query: ", err)
-	// }
-	
-	// fmt.Printf("Hi %s, Welcoma back!\n", result.type2)
 	fmt.Println("Connected to postgres")
 	con = db
 	return db
