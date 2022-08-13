a03ca68f07583748514d656d7cc11b825547ae25
juandreww <juandreww@outlook.com>
2022-08-13 21:15:31 +0700

    asdf

--- server.go
+++ server.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"database/sql"
+	_ "github.com/lib/pq"
+)
 
-
-	"github.com/gofiber/fiber/v2"
+const (
+	host = "ec2-52-73-155-171.compute-1.amazonaws.com"
+	port = 5432
+	user = "'awtpbzyctlpydm'"
+	password = "a7bf40c39496f73a03e7412befbc787d29138445d7fce2a34bf31df40cf07d96"
+	dbname = "d4ehughfapgq0k"
 )
 
 func main() {
-	app := fiber.New()
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "80"
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+    "password=%s dbname=%s sslmode=require",
+    host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
 	}
+	defer db.Close()
+	
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected successfully")
+	
 }
