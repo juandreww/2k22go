dcd64febc0debea28e90c7331b4edecd0c78ea85
juandreww <juandreww@outlook.com>
2022-08-13 21:05:40 +0700

    asdf

--- server.go
+++ server.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+}
