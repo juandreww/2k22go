7a2a72fd439c78051f576758db1d2c74c4151dd4
juandreww <juandreww@outlook.com>
2022-08-13 20:49:47 +0700

    asdf

--- controller/create.go
+++ controller/create.go
@@ -10,7 +10,6 @@ import (
 func create() http.HandlerFunc {
 	fmt.Println("create")
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(r.Method)
 		if r.Method == http.MethodPost {
 			if err := model.CreateKelapa(); err != nil {
 				w.Write([]byte("Some error"))
