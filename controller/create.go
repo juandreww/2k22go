fa7daf7bbea52f52c64a17255b96c02d839af00f
juandreww <juandreww@outlook.com>
2022-08-14 09:17:18 +0700

    create

--- controller/create.go
+++ controller/create.go
@@ -4,11 +4,9 @@ import (
 	"net/http"
 	// "encoding/json"
 	"2k22go/model"
-	"fmt"
 )
 
 func create() http.HandlerFunc {
-	fmt.Println("create")
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			if err := model.CreateKelapa(); err != nil {
