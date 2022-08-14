6a540ba3e38cfaf1b96fbf06f542fa8405320ec6
juandreww <juandreww@outlook.com>
2022-08-14 18:02:52 +0700

    asdf

--- controller/create.go
+++ controller/create.go
@@ -15,16 +15,19 @@ func create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 			data := views.Kelapa{}
-			log.Println(data)
 			json.NewDecoder(r.Body).Decode(&data)
-			log.Println(r.Body)
-			log.Println(data)
-			spew.Dump(r.Body)
 			if err := model.CreateKelapa(data.Type2, data.Quantity); err != nil {
 				w.Write([]byte("Some error"))
 				return
 			}
 			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(data)
+		} else if r.Method == http.MethodGet {
+			data, err := model.ReadAll()
+			if err != nil {
+				w.Write([]byte(err.Error()))
+			}
+			w.WriteHeader(http.StatusOK)
 			json.NewEncoder(w).Encode(data)
 		}
 	}
