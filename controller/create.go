80a816f0a4c3ebf82c30823758e3533e80b06bde
juandreww <juandreww@outlook.com>
2022-08-14 16:11:27 +0700

    zxcv

--- controller/create.go
+++ controller/create.go
@@ -2,40 +2,20 @@ package controller
 
 import (
 	"net/http"
-	"encoding/json"
-	// "2k22go/model"
-	"2k22go/views"
-	"fmt"
+	// "encoding/json"
+	"2k22go/model"
+	// "2k22go/views"
+	// "fmt"
 
 )
 
 func create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-			fmt.Println("Request received")
-			data := views.Response{
-				Code : http.StatusOK,
-				Body : "pong",
+			if err := model.CreateKelapa(); err != nil {
+				w.Write([]byte("Some error"))
+				return
 			}
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(data)
-		} else {
-			fmt.Println("Request different")
-			data := views.Response{
-				Code : http.StatusOK,
-				Body : "pong different",
-			}
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(data)
 		}
 	}
-
-	// return func(w http.ResponseWriter, r *http.Request) {
-	// 	if r.Method == http.MethodGet {
-	// 		if err := model.CreateKelapa(); err != nil {
-	// 			w.Write([]byte("Some error"))
-	// 			return
-	// 		}
-	// 	}
-	// }
 }
