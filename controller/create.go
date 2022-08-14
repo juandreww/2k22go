7a8fdaaf067ff1dff9db427933b8420687f75e34
juandreww <juandreww@outlook.com>
2022-08-14 16:14:28 +0700

    asdf

--- controller/create.go
+++ controller/create.go
@@ -2,9 +2,9 @@ package controller
 
 import (
 	"net/http"
-	// "encoding/json"
+	"encoding/json"
 	"2k22go/model"
-	// "2k22go/views"
+	"2k22go/views"
 	// "fmt"
 
 )
@@ -12,10 +12,14 @@ import (
 func create() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-			if err := model.CreateKelapa(); err != nil {
+			data := views.Kelapa{}
+			json.NewDecoder(r.Body).Decode(&data)
+			if err := model.CreateKelapa(data.type2, data.quantity); err != nil {
 				w.Write([]byte("Some error"))
 				return
 			}
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(data)
 		}
 	}
 }
