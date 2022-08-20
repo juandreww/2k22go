b84348e601d5a302d4cfa001fa0c96e2b89561e0
juandreww <juandreww@outlook.com>
2022-08-20 13:22:16 +0700

    handle bar

--- 01x0_handsonexercise/main.go
+++ 01x0_handsonexercise/main.go
@@ -1,57 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"net/http"
 )
 
-var x int
-
-type person struct {
-	fname string
-	lname string
-}
-
-type secretAgent struct {
-	person
-	allowedtobringgun bool
-}
-
-func (p person) speak() {
-	fmt.Println(p.fname, p.lname, `says, "Good morning, James."`)
-}
-
-func (p secretAgent) speak() {
-	fmt.Println(p.fname, p.lname, `says, "Good morning, James."`)
-}
-
-type human interface {
-	speak()
-}
-
-func saySomething(h human) {
-	h.speak()
-}
-
 func main() {
-	p1 := person{
-		"Miss",
-		"MoneyPenny",
-	}
-	fmt.Println(p1)
-	p1.speak()
-
-	sa1 := secretAgent{
-		person{
-			"Donny",
-			"Yen",
-		},
-		true,
-	}
-	fmt.Println(sa1)
-	sa1.speak()
-	sa1.person.speak()
-	fmt.Println()
-
-	saySomething(p1)
-	saySomething(sa1)
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog", bar)
+	http.ListenAndServe(":8080", nil)
 }
