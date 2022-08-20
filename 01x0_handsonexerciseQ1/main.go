fe9c247fce179fffaad900421b5e1c9a4cfa8341
juandreww <juandreww@outlook.com>
2022-08-20 19:26:46 +0700

    ggg

--- 01x0_handsonexerciseQ1/main.go
+++ 01x0_handsonexerciseQ1/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 )
 
+/*
+	tentukan nama pacar
+	hitung ukuran kotak kado untuk pacar
+	hitung ukuran cincin untuk pacar
+	print out diameter jari manis pacar
+	print out keliling cincin, dan luas area kotak
+*/
+
+type girlfriend struct {
+	fName string
+	lName string
+}
+
 func main() {
+	myGf := girlfriend{
+		"Yomelia",
+		"Christine",
+	}
+	fmt.Println("My girlfriend name is: ")
+	fmt.Println(myGf)
+
 	// create type square
-	
+
 	
 	// http.HandleFunc("/", foo)
 	// http.HandleFunc("/dog", bar)
