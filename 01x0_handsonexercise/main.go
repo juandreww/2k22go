718dd97009713f62ce22d7ecf76ad7c545f5dccd
juandreww <juandreww@outlook.com>
2022-08-20 13:24:14 +0700

    coconut

--- 01x0_handsonexercise/main.go
+++ 01x0_handsonexercise/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"io"
 	"net/http"
+	"fmt"
 )
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "foo ran")
+}
+
+func bar(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "bar ran")
+}
 
 func main() {
 	http.HandleFunc("/", foo)
 	http.HandleFunc("/dog", bar)
+
+	fmt.Println("Serving...");
 	http.ListenAndServe(":8080", nil)
 }
