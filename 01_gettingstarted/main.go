acbe2c81cdb2422dd67036aa93549e84edec2b23
juandreww <juandreww@outlook.com>
2022-08-20 11:42:19 +0700

    asdf

--- 01_gettingstarted/main.go
+++ 01_gettingstarted/main.go
@@ -10,4 +10,13 @@ func main() {
 	x := 7
 	// started(x)
 	fmt.Println(x)
+
+	xi := []int{1,2,3,4}
+	fmt.Println(xi)
+
+	mp := map[string]int {
+		"Boy" : 10,
+		"Girl" : 10,
+	}
+	fmt.Println(mp)
 }
