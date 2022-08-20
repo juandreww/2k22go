9cdac839a2d72d9941c3585a1e56be9a6309e75b
juandreww <juandreww@outlook.com>
2022-08-20 22:33:06 +0700

    ggg

--- 01x1_5of17exercise2/main.go
+++ 01x1_5of17exercise2/main.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	name string
+	location string
+	fruits []string
+}
+
 func main() {
-	fmt.Println("print")
+	p1 := Person{
+		"Larasati",
+		"Farm",
+		[]string{"apple", "banana", "cherry", "grape", "pears",},
+	}
+
+	fmt.Println(p1)
+	for k, v := range p1.fruits {
+		fmt.Println("index ke", k, "isinya", v)
+	}
 }
