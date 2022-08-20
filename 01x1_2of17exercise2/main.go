194ee91edf9b844c7a5de565af295e7d29518ee2
juandreww <juandreww@outlook.com>
2022-08-20 22:10:02 +0700

    vbvv

--- 01x1_2of17exercise2/main.go
+++ 01x1_2of17exercise2/main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+var farm map[string]int
+
 func main() {
-	fmt.Println("saya di urutan ke dua")
+	farm = map[string]int{}
+
+	farm["dog"] = 1
+	farm["chicken"] = 177
+	farm["cow"] = 88
+	farm["fish"] = 256
+	farm["goat"] = 33
+
+	fmt.Println("Jumlah anjing adalah", farm["dog"])
 }
