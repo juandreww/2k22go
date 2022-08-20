448d8918b8783fec41230ffdc9d6673a099e701b
juandreww <juandreww@outlook.com>
2022-08-20 11:40:58 +0700

    bcbc

--- 01_gettingstarted/main.go
+++ 01_gettingstarted/main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	// "encoding/json"
-	"00_udemygointro/controller"
-	"00_udemygointro/model"
-	// "log"
-	// "os"
 )
 
+var x int
+
 func main() {
-	mux := controller.RegisterApi()
-	db := model.ConnectDB()
-	defer db.Close()
-	
-	fmt.Println("Serving...");
-
-	// for heroku
-	// port := os.Getenv("PORT")
-	// log.Fatal(http.ListenAndServe(":"+port, mux))
-
-	// for Local
-	http.ListenAndServe(":80", mux)
+	x := 7
+	// started(x)
+	fmt.Println(x)
 }
