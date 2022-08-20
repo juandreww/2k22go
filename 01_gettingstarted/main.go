1bdc107ee6ab0b737afc327859194715a8473e93
juandreww <juandreww@outlook.com>
2022-08-20 13:18:51 +0700

    main.go

--- 01_gettingstarted/main.go
+++ 01_gettingstarted/main.go
@@ -24,6 +24,14 @@ func (p secretAgent) speak() {
 	fmt.Println(p.fname, p.lname, `says, "Good morning, James."`)
 }
 
+type human interface {
+	speak()
+}
+
+func saySomething(h human) {
+	h.speak()
+}
+
 func main() {
 	p1 := person{
 		"Miss",
@@ -41,4 +49,9 @@ func main() {
 	}
 	fmt.Println(sa1)
 	sa1.speak()
+	sa1.person.speak()
+	fmt.Println()
+
+	saySomething(p1)
+	saySomething(sa1)
 }
