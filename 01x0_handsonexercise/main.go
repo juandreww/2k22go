5d144419f8d6049dea4c79f38c4522c131ccd19d
juandreww <juandreww@outlook.com>
2022-08-20 18:34:18 +0700

    fff

--- 01x0_handsonexercise/main.go
+++ 01x0_handsonexercise/main.go
@@ -20,30 +20,16 @@ type person struct {
 }
 
 func main() {
-	aa := make(map[string]string)
-	aa["James"] = "007";
-	fmt.Println(aa["James"])
-
-
-	p1 := person{"Nina", "Dobrev",}
-	p2 := person{"Alicia", "Key",}
-	fmt.Println(p1)
-	fmt.Println(p2)
-
-	a1 := struct{
-		breed string
-		name string
-	}{
-		"Holywings",
-		"Shaun",
-	}
-
-	fmt.Println(a1)
-
+	m := meaning()
+	fmt.Println(m)
 
 	// http.HandleFunc("/", foo)
 	// http.HandleFunc("/dog", bar)
 
 	// fmt.Println("Serving...");
 	// http.ListenAndServe(":8080", nil)
+}
+
+func meaning() int {
+	return 42
 }
