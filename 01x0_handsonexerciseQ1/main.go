87d9746c3104f22d2f635637409f21397ffc9c98
juandreww <juandreww@outlook.com>
2022-08-20 18:53:17 +0700

    hjhj

---
+++ 01x0_handsonexerciseQ1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	// "fmt"
+)
+
+func main() {
+	// create type square
+	
+	
+	// http.HandleFunc("/", foo)
+	// http.HandleFunc("/dog", bar)
+
+	// fmt.Println("Serving...");
+	// http.ListenAndServe(":8080", nil)
+}
