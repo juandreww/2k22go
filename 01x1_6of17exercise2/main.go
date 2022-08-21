a3c0d04c251bbad5de1671f2f78e732bbac4683d
juandreww <juandreww@outlook.com>
2022-08-21 09:08:16 +0700

    asdf

--- 01x1_6of17exercise2/main.go
+++ 01x1_6of17exercise2/main.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+type Person struct {
+	fName string
+	lName string
+}
+
+func (p Person) walk() string {
+	return p.fName + "is walking"
+}
+
 func main() {
-	fmt.Println("im here")
+	p1 := Person {
+		"Donny",
+		"Yen",
+	}
+	fmt.Sprintln(p1.walk())
 }
