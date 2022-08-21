5efbf71fba22702171985802042ee697f837bc6d
juandreww <juandreww@outlook.com>
2022-08-21 09:26:48 +0700

    gogo

---
+++ 01x1_8of17exercise2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type vehicle struct {
+	doors string
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+	fmt.Println("hello")
+}
