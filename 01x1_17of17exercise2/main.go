f30f1fa6c46bad097cb05a8fc20f47bb69fc43da
juandreww <juandreww@outlook.com>
2022-08-21 14:56:00 +0700

    gggh

--- 01x1_17of17exercise2/main.go
+++ 01x1_17of17exercise2/main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("apalo")
+	s := "I'd rather eat the vegetables than meat"
+	fmt.Println(s)
+	fmt.Println([]byte(s))
+	fmt.Println(string([]byte(s)))
+	fmt.Println(string([]byte(s)[:14]))
+	// fmt.Println(s)
+	// fmt.Println(s)
+	// fmt.Println(s)
 }
