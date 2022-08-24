8480e747ed61ba0e1a9f922af8d691d0bdf6e69c
juandreww <juandreww@outlook.com>
2022-08-24 07:23:55 +0700

    strings

---
+++ 02_templateconcat/02_file/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"strings"
+)
+
+func main() {
+	name := "Victor Schotleim Reinbach III"
+	str := fmt.Sprintf(`
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		<meta charset="UTF-8">
+		<title>Hello World!</title>
+		</head>
+
+		<body>
+			<h1>` + name + `</h1>
+		</body>
+		</html>
+	`)
+
+	nf, err := os.Create("index.html")
+	if err != nil {
+		log.Fatal("error creating file", err)
+	}
+	defer nf.Close()
+
+	io.Copy(nf, strings.NewReader(str))
+}
