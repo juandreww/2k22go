55db3c7c9f2ae48e5f96350ceaf73e3c41c8450f
juandreww <juandreww@outlook.com>
2022-08-24 21:21:46 +0700

    zxcv

--- 02x0_templatepackage/03_ParseFiles/main.go
+++ 02x0_templatepackage/03_ParseFiles/main.go
@@ -17,6 +17,21 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	tpl, err = template.ParseFiles("two.gofile", "three.gofile")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gofile", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "three.gofile", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	nf, err := os.Create("index.html")
 	if err != nil {
 		log.Println("error creating file", err)
