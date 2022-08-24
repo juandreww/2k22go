7188c5acd2d4324204abc329d113c1e54eaf2e9e
juandreww <juandreww@outlook.com>
2022-08-24 21:53:08 +0700

    asdf

---
+++ 02x0_templatepackage/04_ParseGlob/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseGlob("templates/*")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "three.gofile", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "two.gofile", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "one.gofile", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+
+}
