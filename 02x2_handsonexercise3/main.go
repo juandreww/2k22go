0575ba8f015d20f5a4da0ebee1d36bc06fc924f0
juandreww <juandreww@outlook.com>
2022-08-27 22:07:46 +0700

    asdf

--- 02x2_handsonexercise3/main.go
+++ 02x2_handsonexercise3/main.go
@@ -4,6 +4,85 @@ import (
 	"fmt"
 )
 
+type Item struct {
+	Name string
+	Quantity int
+}
+
+type Period struct {
+	Name string
+	Item []Item
+}
+
+type DQCoffee struct {
+	Period []Period
+}
+
 func main() {
-	fmt.Println("hello world")
+	// DQ Coffee 24 / 7
+	dq := DQCoffee {
+		[]Period {
+			Period{
+				"Breakfast",
+				[]Item {
+					Item {
+						"Telur Rebus",
+						10,
+					},
+					Item {
+						"Ayam Rebus",
+						10,
+					},
+					Item {
+						"Sosis Goreng",
+						10,
+					},
+				},
+			},
+			Period{
+				"Lunch",
+				[]Item {
+					Item {
+						"Ayam Penyet",
+						10,
+					},
+					Item {
+						"Tahu, Tempe, Telur",
+						10,
+					},
+					Item {
+						"Indomie Goreng",
+						10,
+					},
+					Item {
+						"Indomie Kuah",
+						10,
+					},
+				},
+			},
+			Period{
+				"Dinner",
+				[]Item {
+					Item {
+						"Teh Susu",
+						10,
+					},
+					Item {
+						"Kopi Susu",
+						10,
+					},
+					Item {
+						"Matcha Latte",
+						10,
+					},
+					Item {
+						"Wedang Jahe",
+						10,
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Println(dq)
 }
