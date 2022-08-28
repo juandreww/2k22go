d71e2a08bca7152e8e8da6a13a5bc54070c637f6
juandreww <juandreww@outlook.com>
2022-08-28 14:09:27 +0700

    read csv file

--- 02x2_handsonexercise5/main.go
+++ 02x2_handsonexercise5/main.go
@@ -2,179 +2,43 @@ package main
 
 import (
 	// "fmt"
+	"encoding/csv"
 	"text/template"
 	"os"
 	"log"
 	// "reflect"
 )
-type Item struct {
-	Name string
-	Quantity int
-}
 
-type MealPeriod struct {
-	Name string
-	Item []Item
-}
-
-type DQCoffee struct {
-	MealPeriod []MealPeriod
-}
-
-type Branch struct {
-	BranchName string
-	DQCoffee []DQCoffee
+type Commodity struct {
+	Location, Name string
 }
 
 var tpl *template.Template
 
-var fm = template.FuncMap{
-	"add": func(a, b int) int {
-		return a + b
-	},
-}
+// var fm = template.FuncMap{
+// 	"add": func(a, b int) int {
+// 		return a + b
+// 	},
+// }
 
 func init() {
-	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").ParseFiles("tpl.gohtml"))
 }
 
 func main() {
-	// DQ Coffee 24 / 7
-	dqc := Branch {
-		"Batam Centre",
-		[]DQCoffee {
-			DQCoffee {
-				[]MealPeriod {
-					MealPeriod{
-						"Breakfast",
-						[]Item {
-							Item {
-								"Telur Rebus",
-								10,
-							},
-							Item {
-								"Ayam Rebus",
-								10,
-							},
-							Item {
-								"Sosis Goreng",
-								10,
-							},
-						},
-					},
-					MealPeriod{
-						"Lunch",
-						[]Item {
-							Item {
-								"Ayam Penyet",
-								10,
-							},
-							Item {
-								"Tahu, Tempe, Telur",
-								10,
-							},
-							Item {
-								"Indomie Goreng",
-								10,
-							},
-							Item {
-								"Indomie Kuah",
-								10,
-							},
-						},
-					},
-					MealPeriod{
-						"Dinner",
-						[]Item {
-							Item {
-								"Teh Susu",
-								10,
-							},
-							Item {
-								"Kopi Susu",
-								10,
-							},
-							Item {
-								"Matcha Latte",
-								10,
-							},
-							Item {
-								"Wedang Jahe",
-								10,
-							},
-						},
-					},
-				},
-			},
-			DQCoffee {
-				[]MealPeriod {
-					MealPeriod{
-						"Breakfast",
-						[]Item {
-							Item {
-								"Nasi Kucing",
-								10,
-							},
-							Item {
-								"Nasi Lemak",
-								10,
-							},
-							Item {
-								"Bubur Ayam",
-								10,
-							},
-						},
-					},
-					MealPeriod{
-						"Lunch",
-						[]Item {
-							Item {
-								"Soto Solo",
-								10,
-							},
-							Item {
-								"Soto Daging",
-								10,
-							},
-							Item {
-								"Soto Rempah",
-								10,
-							},
-							Item {
-								"Soto Kecap",
-								10,
-							},
-						},
-					},
-					MealPeriod{
-						"Dinner",
-						[]Item {
-							Item {
-								"Teh Milkshake",
-								10,
-							},
-							Item {
-								"Coffee Milk",
-								10,
-							},
-							Item {
-								"Coffee Latte",
-								10,
-							},
-							Item {
-								"Coffee Arabica",
-								10,
-							},
-						},
-					},
-				},
-			},
-		},
+	f, err := os.Open("hargakomoditas.csv")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	// fmt.Println(dqc)
+	defer f.Close()
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", dqc)
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", f)
 	if err != nil {
 		log.Fatalln(err)
 	}
