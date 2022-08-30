26880280ba5d471c07b3ac622db4ccd74f9412d3
juandreww <juandreww@outlook.com>
2022-08-30 20:00:58 +0700

    JAMAL

--- 03x6_approt13/main.go
+++ 03x6_approt13/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
 )
 
 func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
 	
 }
