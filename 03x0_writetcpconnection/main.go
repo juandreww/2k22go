6b89470fcbb632b9af473991c017386f5b41d087
juandreww <juandreww@outlook.com>
2022-08-30 07:25:28 +0700

    asd

--- 03x0_writetcpconnection/main.go
+++ 03x0_writetcpconnection/main.go
@@ -14,17 +14,21 @@ func main() {
 	}
 	defer li.Close()
 
+	fmt.Println("Phase 1")
 	for {
 		// LAC : Listen Accept Connect
+		fmt.Println("Phase 2")
 		conn, err := li.Accept()
 		if err != nil {
 			log.Println(err)
 		}
 
+		fmt.Println("Phase 3")
 		io.WriteString(conn, "\nHello from TCP Server\n")
 		fmt.Fprintln(conn, "How is your day?")
 		fmt.Fprintf(conn, "%v", "Well I Hope")
-		
+
+		fmt.Println("Phase 4")
 		conn.Close()
 	}
 }
