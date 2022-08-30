fef40c1e5881f46f7147b7bfa784c9e861aeaefa
juandreww <juandreww@outlook.com>
2022-08-30 19:09:18 +0700

    ccc

--- 03x1_readtcpconnection/main.go
+++ 03x1_readtcpconnection/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-
+	"bufio"
+	"fmt"
+	"log"
+	"net"
 )
 
 func main() {
-	
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer li.Close()
 }
