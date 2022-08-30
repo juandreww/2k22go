12bb26f2158d935ed1a3d2e12e1914251c1457ff
juandreww <juandreww@outlook.com>
2022-08-30 19:33:50 +0700

    asdf

--- 03x4_dialreadtcp/main.go
+++ 03x4_dialreadtcp/main.go
@@ -1,44 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
-	"time"
 )
 
 func main() {
-	li, err := net.Listen("tcp", ":8080")
+	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	defer li.Close()
-
-	for {
-		conn, err := li.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		go handle(conn)
-	}
-}
-
-func handle(conn net.Conn) {
-	err := conn.SetDeadline(time.Now().Add(30 * time.Second))
-	if err != nil {
-		log.Fatalln("CONN TIMEOUT")
-	}
-
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println("he says " + ln)
-		fmt.Fprintf(conn, "I heard what you say : %s\n", ln)
-	}
 	defer conn.Close()
 
-	fmt.Println("OK YOUR 30 SECONDS IS UP")
+	bs, err := ioutil.ReadAll(conn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(bs))
 }
