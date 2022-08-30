7ea8787880a3ed9a84262be0a00de0e7023e9128
juandreww <juandreww@outlook.com>
2022-08-30 07:15:54 +0700

    asdf

--- 03x0_writetcpconnection/main.go
+++ 03x0_writetcpconnection/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-
+	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
 func main() {
-	
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		io.WriteString(conn, "\nHello from TCP Server\n")
+	}
 }
