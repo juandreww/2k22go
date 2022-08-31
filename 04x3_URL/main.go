b0ed5a1f57701938c2e34155d7b654500765f5fc
juandreww <juandreww@outlook.com>
2022-09-01 06:17:33 +0700

    GO HTML

---
+++ 04x3_URL/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type handlerlemon float64
+
+func (p handlerlemon) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method string
+		Submissions url.Values
+	}{
+		req.Method,
+		req.Form,
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var p handlerlemon
+	http.ListenAndServe(":8080", p)
+}
