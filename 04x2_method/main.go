2f7cbdde11a5ac61fdacd0924e11300d4200c625
juandreww <juandreww@outlook.com>
2022-09-01 05:46:44 +0700

    asdf

--- 04x2_method/main.go
+++ 04x2_method/main.go
@@ -7,6 +7,30 @@ import (
 	"net/url`"
 )
 
+type handlerlemon float64
+
+func (p handlerlemon) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method string
+		Submissions url.Values
+	}{
+		req.Method,
+		req.Form,
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = 
+}
 
 func main() {
 
