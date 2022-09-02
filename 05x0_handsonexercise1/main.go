2c22b85323cabfa068d27449c8dacbce1cd2fa18
juandreww <juandreww@outlook.com>
2022-09-02 07:07:36 +0700

    me

--- 05x0_handsonexercise1/main.go
+++ 05x0_handsonexercise1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 )
 
 type handler1 int
@@ -11,10 +12,22 @@ func (p handler1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>My Name is Andrew</h1>")
 }
 
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "here is the index")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "here is the dog")
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "my name is Andrew")
+}
+
 func main() {
 	mux := http.DefaultServeMux
-	mux.HandleFunc("/", index())
-	mux.HandleFunc("/dog/", dog())
-	mux.HandleFunc("/me/", me())
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/dog/", dog)
+	mux.HandleFunc("/me/", me)
 	http.ListenAndServe(":8080", mux)
 }
