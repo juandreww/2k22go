131dc44527a7ae6d63a10775d2c7fd0fd51e67f1
juandreww <juandreww@outlook.com>
2022-09-02 07:35:08 +0700

    go run main.go

--- 05x1_handsonexercise2/main.go
+++ 05x1_handsonexercise2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"io"
+	"html/template"
 )
 
 type handler1 int
@@ -18,10 +19,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func dog(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>here is the dog</h1>")
+
+	data := "here is the dog"
+
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
 }
 
 func me(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>my name is Andrew</h1>")
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
