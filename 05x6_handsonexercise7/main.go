64b736c486d9bd943b95142a530e08083f17665b
juandreww <juandreww@outlook.com>
2022-09-03 20:35:15 +0700

    zxcv

--- 05x6_handsonexercise7/main.go
+++ 05x6_handsonexercise7/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"log"
+	"bufio"
+	"strings"
 )
 
 func main() {
@@ -28,4 +30,21 @@ func main() {
 
 func serve(conn net.Conn) {
 	defer conn.Close()
+	var i int
+	var rMethod, rURL string
+	scanner := bufio.newScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			xs := strings.Fields(ln)
+			rMethod = xs[0]
+			rURL = xs[1]
+			fmt.Println("METHOD:", rMethod)
+			fmt.Println("URL:", rURL)
+		}
+		if ln == "" {
+
+		}
+	}
 }
