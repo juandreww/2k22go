3c0bcd4969414381760247f179af426bc618c662
juandreww <juandreww@outlook.com>
2022-09-03 12:06:10 +0700

    asdf

--- 05x2_handsonexercise3/main.go
+++ 05x2_handsonexercise3/main.go
@@ -22,23 +22,17 @@ func main() {
 			log.Println(err)
 			continue
 		}
-		go handle(conn)
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			ln := scanner.Text()
+			fmt.Println(ln)
+		}
+		defer conn.Close()
 
-		
+		fmt.Println("code got here")
 		io.WriteString(conn, "Hello from TCP server\n")
 		fmt.Fprintln(conn, "Okay youre done")
 
 		conn.Close()
 	}
-}
-
-func handle(conn net.Conn) {
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		ln := scanner.Text()
-		fmt.Println(ln)
-	}
-	defer conn.Close()
-
-	fmt.Println("code got here")
 }
