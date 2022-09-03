f928650846d59235f5b2c753742d327eb33aa740
juandreww <juandreww@outlook.com>
2022-09-03 11:43:42 +0700

    youre in our shop

--- 05x2_handsonexercise3/main.go
+++ 05x2_handsonexercise3/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("day after day")
+	mux := http.NewServeMux()
+	mux.Handle("/warehouse", http.HandlerFunc(warehouse))
+	mux.Handle("/factory", http.HandlerFunc(factory))
+	mux.Handle("/shop", http.HandlerFunc(shop))
+
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func warehouse(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hi, youre in our warehouse.")
+}
+
+func factory(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hi, youre in our factory.")
+}
+
+func shop(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hi, youre in our shop.")
 }
