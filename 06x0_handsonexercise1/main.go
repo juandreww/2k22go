d31f6ebe3d3a71741a97671fdea5bf6aa3e94e11
juandreww <juandreww@outlook.com>
2022-09-03 22:08:02 +0700

    snoopy1

--- 06x0_handsonexercise1/main.go
+++ 06x0_handsonexercise1/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", minilemon())
+	mux.Handle("/dog/", dog())
 	fmt.Println("hello")
+
+	http.ListenAndServe(":8080", mux)
+}
+
+func minilemon(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Mini Lemon toy found here")
+}
+
+func dog(w http.ResponseWriter, r *http.Request) {
+	
 }
