194a4901ef1c6c4e99977d4c4c1ddd1920840585
juandreww <juandreww@outlook.com>
2022-09-03 14:05:07 +0700

    wen i was just a little girl

---
+++ 05x5_handsonexercise6/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	// "net/http"
+	"net"
+	"io"
+	"log"
+	"bufio"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		serve(conn)
+	}
+}
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	i := 0;
+	var rMethod, rURI string
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			xs := strings.Fields(ln)
+			rMethod = xs[0]
+			rURI = xs[1]
+			fmt.Println("METHOD:", rMethod)
+			fmt.Println("URL:", rURI)
+		}
+		if ln == "" {
+			fmt.Println(conn, "Information of our shop is all")
+			break
+		}
+		i++
+	}
+	
+	body := "CHECK OUT THE RESPONSE BODY PAYLOAD"
+	body += "\n"
+	body += rMethod
+	body += "\n"
+	body += rURI
+	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	io.WriteString(conn, "\r\n")
+	io.WriteString(conn, body)
+}
