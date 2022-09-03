a9a18bef4e7fc8cf19d0a721ac4f881b022feefc
juandreww <juandreww@outlook.com>
2022-09-03 22:10:35 +0700

    io

--- 06x0_handsonexercise1/main.go
+++ 06x0_handsonexercise1/main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"html/template"
+	"io"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("dog.gohtml"))
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -19,5 +27,9 @@ func minilemon(w http.ResponseWriter, r *http.Request) {
 }
 
 func dog(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Here is the index</h1>")
 
+	data := "here is the dog"
+
+	tpl.ExecuteTemplate(w, "dog.gohtml", data)
 }
