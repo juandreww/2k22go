5f952c8b401aafc53a643a3a1b64cc1f997860ef
juandreww <juandreww@outlook.com>
2022-09-04 08:20:59 +0700

    zxcv

--- 06x0_handsonexercise1/main.go
+++ 06x0_handsonexercise1/main.go
@@ -1,37 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
-	"html/template"
-	"log"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.HandlerFunc(minilemon))
-	mux.HandleFunc("/dog/", dog)
-	mux.HandleFunc("/snoopy1", ServeSnoopy)
-	fmt.Println("hello")
-
-	http.ListenAndServe(":8080", mux)
+	http.HandleFunc("/", dog)
+	// http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":8080", nil)
 }
 
 func dog(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("dog.gohtml")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tpl.ExecuteTemplate(w, "dog.gohtml", nil)
-}
-
-func minilemon(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Mini Lemon toy found here")
-}
-
-
-
-func ServeSnoopy(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "snoopy1.jpg")
+	io.WriteString(w, `<img src="/resources/dog.jpg">`)
 }
