df1c825916c295c2449dffd86fef799869fb1ef4
juandreww <juandreww@outlook.com>
2022-09-06 19:44:46 +0700

    asdf

--- 08x8_temporaryredirect/main.go
+++ 08x8_temporaryredirect/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"html/template"
-	"log"
-	"net/http"
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
 var tpl *template.Template
@@ -13,28 +12,25 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
-type person struct {
-	FirstName  string
-	LastName   string
-	Subscribed bool
-}
-
 func main() {
 	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-	// body
-	bs := make([]byte, req.ContentLength)
-	req.Body.Read(bs)
-	body := string(bs)
-	fmt.Println(body)
+	fmt.Print("Your request method at foo: ", req.Method, "\n\n")
+}
 
-	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		log.Fatalln(err)
-	}
+func bar(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at bar:", req.Method)
+	// process form submission here
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at barred:", req.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
