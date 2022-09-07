b6495d1a1a091d07e062b29ddf553b8104cb2f59
juandreww <juandreww@outlook.com>
2022-09-07 20:02:05 +0700

    index go html

--- 09x1_session/main.go
+++ 09x1_session/main.go
@@ -15,11 +15,12 @@ func init() {
 func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/welcome", welcome)
-	mux.HandleFunc("/submit", submit)
+	// mux.HandleFunc("/submit", submit)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	
+	fmt.Println("Welcome to The Lemon Bar. Your used method: ", r.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
