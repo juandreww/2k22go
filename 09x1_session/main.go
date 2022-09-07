f1fa2850bb520b2c053dacc28e4da850528eb4b5
juandreww <juandreww@outlook.com>
2022-09-07 20:30:31 +0700

    asdf

--- 09x1_session/main.go
+++ 09x1_session/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"github.com/google/uuid"
+	"log"
 )
 
 var tpl *template.Template
@@ -25,6 +27,7 @@ func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/welcome", welcome)
 	mux.HandleFunc("/submit", submit)
+	mux.HandleFunc("/atthebar", atthebar)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
@@ -38,4 +41,40 @@ func submit(w http.ResponseWriter, r *http.Request) {
 	fname :=  r.FormValue("firstname")
 	lname := r.FormValue("lastname")
 	email := r.FormValue("email")
+	uuid := uuid.New()
+	dbUser[uuid.String()] = user {
+		fname,
+		lname,
+		email,
+	}
+
+	cookie := &http.Cookie {
+		Name:  "session-id",
+		Value: uuid.String(),
+		HttpOnly: true,
+		// Secure: true,
+	}
+	http.SetCookie(w, cookie)
+	http.Redirect(w, r, "/atthebar", http.StatusSeeOther)
+}
+
+func atthebar(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session-id")
+	if err != nil {
+		HandleError(w, err)
+	}
+
+	u := dbUser[cookie.Value]
+	if u == (user{}) {
+		http.Redirect(w, r, "/welcome", http.StatusSeeOther)
+	}
+	
+	tpl.ExecuteTemplate(w, "thebar.gohtml", nil)
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
 }
