fda59ff19f34ac865da9386c7345079676ae29cb
juandreww <juandreww@outlook.com>
2022-09-07 21:49:40 +0700

    gogogo

--- 09x3_signup/session.go
+++ 09x3_signup/session.go
@@ -7,6 +7,7 @@ import (
 
 func alreadySignup(req *http.Request) bool {
 	cookie, err := req.Cookie("session-id")
+	
 	if err != nil {
 		return false
 	}
@@ -18,9 +19,14 @@ func alreadySignup(req *http.Request) bool {
 
 func getUser(req *http.Request) user {
 	cookie, err := req.Cookie("session-id")
+	var u user
 	if err != nil {
 		return user{}
 	}
 
-	
+	if un, ok := dbSessions[cookie.Value]; ok {
+		u = dbUser[un]
+	}
+
+	return u
 }
