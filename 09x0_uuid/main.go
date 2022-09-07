6718a34a69902dbcb4f019cd1717df7bd3732e9b
juandreww <juandreww@outlook.com>
2022-09-07 19:55:44 +0700

    main.go

--- 09x0_uuid/main.go
+++ 09x0_uuid/main.go
@@ -8,5 +8,22 @@ import (
 
 func main() {
 	mux := http.DefaultServeMux
+	mux.HandleFunc("/session", session)
+	http.ListenAndServe(":8080", nil)
+}
 
+func session(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session-id")
+	if (err != nil) {
+		uuid := uuid.New()
+		cookie = &http.Cookie{
+			Name:  "session-id",
+			Value: uuid.String(),
+			HttpOnly: true,
+			// Secure: true,
+		}
+		http.SetCookie(w, cookie)
+		fmt.Println("added new cookie")
+	}
+	fmt.Println(cookie)
 }
