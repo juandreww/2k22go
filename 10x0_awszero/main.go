fe0dc538f390d4c35ef1bdc09435b14ca0cb186b
juandreww <juandreww@outlook.com>
2022-09-08 22:11:51 +0700

    parse

--- 10x0_awszero/main.go
+++ 10x0_awszero/main.go
@@ -11,7 +11,7 @@ import (
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
 
 func main() {
