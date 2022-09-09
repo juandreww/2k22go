d0331f5a4848396c52072d7d0816b28dedad041a
juandreww <juandreww@outlook.com>
2022-09-09 19:49:56 +0700

    cookie

--- 10x0_awszero/main.go
+++ 10x0_awszero/main.go
@@ -30,6 +30,10 @@ func init() {
 func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/index", index)
+	mux.HandleFunc("/atthebar", atthebar)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
@@ -49,6 +53,121 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func atthebar(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := r.Cookie("session-id")
+
+	if (!alreadySignup(r))  {
+		fmt.Println("here")
+		http.Redirect(w, r, "/signup", http.StatusSeeOther)
+		return
+	}
+
+	un, ok := dbSessions[cookie.Value]
+	if !ok {
+		fmt.Println("here2")
+		http.Redirect(w, r, "/signup", http.StatusSeeOther)
+		return
+	}
+	u := dbUser[un]
+	if u.Role == "007" {
+		http.Error(w, "007, please dont come to this bar", http.StatusForbidden)
+		return
+	}
+	tpl.ExecuteTemplate(w, "atthebar.gohtml", u)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Signup page: ", r.Method)
+
+	if r.Method == http.MethodPost {
+		fname :=  r.FormValue("firstname")
+		lname := r.FormValue("lastname")
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		role := r.FormValue("role")
+		
+
+		bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		u := user{fname, lname, email, bs, role,}
+
+		uuid := uuid.New()
+		cookie := &http.Cookie {
+			Name:  "session-id",
+			Value: uuid.String(),
+			HttpOnly: true,
+			// Secure: true,
+		}
+
+		http.SetCookie(w, cookie)
+		dbSessions[cookie.Value] = email
+		dbUser[email] = u
+	}
+
+	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Login page: ", r.Method)
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		fmt.Println("password")
+		fmt.Println(password)
+		fmt.Println()
+
+		u, ok := dbUser[email]
+		fmt.Println(u)
+		fmt.Println()
+		fmt.Println(ok)
+		if !ok {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(password))
+		if err != nil {
+			http.Error(w, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+
+		uuid := uuid.New()
+		cookie := &http.Cookie {
+			Name:  "session-id",
+			Value: uuid.String(),
+			HttpOnly: true,
+			// Secure: true,
+		}
+		http.SetCookie(w, cookie)
+		dbSessions[cookie.Value] = email
+		http.Redirect(w, r, "/atthebar", http.StatusSeeOther)
+	}
+	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := r.Cookie("session-id")
+	if (!alreadySignup(r))  {
+		fmt.Println("here")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	delete(dbSessions, cookie.Value)
+	cookie = &http.Cookie{
+		Name:   "session-id",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, cookie)
+
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 
 func HandleError(w http.ResponseWriter, err error) {
