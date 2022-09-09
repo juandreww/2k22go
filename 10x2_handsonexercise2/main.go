10b60ccdcfaf0b8a6723f2875ace871ff374a535
juandreww <juandreww@outlook.com>
2022-09-10 06:04:30 +0700

    panic attack

--- 10x2_handsonexercise2/main.go
+++ 10x2_handsonexercise2/main.go
@@ -1,52 +1,20 @@
 package main
 
 import (
-    "embed"
-    "html/template"
-    "log"
     "net/http"
-)
-
-var (
-    //go:embed templates/*.gohtml
-    res embed.FS
-    pages = map[string]string{
-        "/index": "templates/index.gohtml",
-		"/signup": "templates/signup.gohtml",
-		"/login": "templates/login.gohtml",
-		"/logout": "templates/logout.gohtml",
-		"/atthebar": "templates/atthebar.gohtml",
-    }
+    "io"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        page, ok := pages[r.URL.Path]
-        if !ok {
-            w.WriteHeader(http.StatusNotFound)
-            return
-        }
-        tpl, err := template.ParseFS(res, page)
-        if err != nil {
-            log.Printf("page %s not found in pages cache...", r.RequestURI)
-            w.WriteHeader(http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Content-Type", "text/html")
-        w.WriteHeader(http.StatusOK)
-        data := map[string]interface{}{
-            "userAgent": r.UserAgent(),
-        }
-        if err := tpl.Execute(w, data); err != nil {
-            return
-        }
-    })
-
-    http.FileServer(http.FS(res))
-    log.Println("server started...")
-    err := http.ListenAndServe(":8080", nil)
+    mux := http.DefaultServeMux
+    mux.HandleFunc("/index", index)
+    err := http.ListenAndServe(":80", nil)
     if err != nil {
         panic(err)
     }
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Oh yeah, I'm running on AWS.")
 }
 
