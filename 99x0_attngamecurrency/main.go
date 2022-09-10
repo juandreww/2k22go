a5ffa8bc2043b13ebf701be05ed6d3b46467cb4d
juandreww <juandreww@outlook.com>
2022-09-10 13:16:06 +0700

    go html

--- 99x0_attngamecurrency/main.go
+++ 99x0_attngamecurrency/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+	"log"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
 
 func main() {
 	fmt.Println("hello")
