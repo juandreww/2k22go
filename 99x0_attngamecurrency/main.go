59cd79c72dd3c5feb1873f138c29d488bdfd0532
juandreww <juandreww@outlook.com>
2022-09-10 13:27:28 +0700

    currency

--- 99x0_attngamecurrency/main.go
+++ 99x0_attngamecurrency/main.go
@@ -7,6 +7,11 @@ import (
 	// "log"
 )
 
+type currency struct {
+	ID string
+	Name string
+}
+
 var tpl *template.Template
 
 func init() {
@@ -16,6 +21,7 @@ func init() {
 func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/savecurrency", savecurrency)
 
 
 	http.Handle("/favicon.ico", http.NotFoundHandler())
@@ -25,5 +31,11 @@ func main() {
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("This is index api: ", r.Method)
 
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func savecurrency(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is savecurrency api: ", r.Method)
+	
 	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
