7c2909a30a00c8db808b157a451dd9108a76cb6b
juandreww <juandreww@outlook.com>
2022-09-12 07:28:17 +0700

    asdfasdf

--- 10x3_handsonexercise3/main.go
+++ 10x3_handsonexercise3/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
     mux := http.DefaultServeMux
     mux.HandleFunc("/index", index)
+    mux.HandleFunc("/index2", index2)
     mux.HandleFunc("/", index)
     err := http.ListenAndServe(":80", nil)
     if err != nil {
@@ -16,6 +17,10 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Oh yeah, I'm running on AWS.")
+}
+
+func index2(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Oh yeah, I'm running on AWS.")
 }
 
