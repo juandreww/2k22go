0868d45d9d2e62cf9763ac98df4b8a1ad5f77ccd
juandreww <juandreww@outlook.com>
2022-09-13 23:02:52 +0700

    asdf

--- 10x3_handsonexercise3/main.go
+++ 10x3_handsonexercise3/main.go
@@ -2,25 +2,21 @@ package main
 
 import (
     "net/http"
-    "io"
+    // "io"
 )
 
 func main() {
     mux := http.DefaultServeMux
     mux.HandleFunc("/index", index)
-    mux.HandleFunc("/index2", index2)
-    mux.HandleFunc("/", index)
-    err := http.ListenAndServe(":80", nil)
+    err := http.ListenAndServe(":8080", nil)
+
+    check(err)
+    
+}
+
+func check(err error) {
     if err != nil {
         panic(err)
-    }
-}
-
-func index(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Oh yeah, I'm running on AWS.")
-}
-
-func index2(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Oh yeah, I'm running so fast on AWS.")
+    } 
 }
 
