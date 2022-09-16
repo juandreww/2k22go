a974c587ee314a0318da54eb9e0e1462166e52e5
juandreww <juandreww@outlook.com>
2022-09-16 21:36:29 +0700

    asd

--- 12x0_elbzero/main.go
+++ 12x0_elbzero/main.go
@@ -1,40 +1,35 @@
 package main
 
 import (
-    "database/sql"
-    // "fmt"
-    _ "github.com/go-sql-driver/mysql"
-    "io"
-    "net/http"
+	"fmt"
+	"io"
+	"net/http"
 )
 
-var db *sql.DB
-var err error
-
 func main() {
-    db, err = sql.Open("mysql", "aws_procube1:aws_procube1@tcp(aws-procube1.chjuuutofqif.ap-southeast-3.rds.amazonaws.com:3306)/default?charset=utf8")
-    check(err)
-    defer db.Close()
-
-    err = db.Ping()
-    check(err)
-
-    
-    mux := http.DefaultServeMux
-    mux.HandleFunc("/index", index)
-    mux.Handle("/favicon.ico", http.NotFoundHandler())
-    err = http.ListenAndServe(":8080", nil)
-    check(err)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/instance", instance)
+	http.ListenAndServe(":80", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-    _, err = io.WriteString(w, "Successfully Completed")
-    check(err)
+func index(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello from AWS.")
 }
 
-func check(err error) {
-    if err != nil {
-        panic(err)
-    } 
+func ping(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "OK")
 }
 
+func instance(w http.ResponseWriter, req *http.Request) {
+	resp, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	bs := make([]byte, resp.ContentLength)
+	resp.Body.Read(bs)
+	resp.Body.Close()
+	io.WriteString(w, string(bs))
+}
