d84d85f08ec278ffb2c7cfd439d2b675a233e8cd
juandreww <juandreww@outlook.com>
2022-09-17 19:49:02 +0700

    asdfsadf

--- 13x0_photoblog/main.go
+++ 13x0_photoblog/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"github.com/google/uuid"
 )
 
 var tpl *template.Template
@@ -16,23 +15,11 @@ func init() {
 func main() {
 	mux := http.DefaultServeMux
 	mux.HandleFunc("/index", index)
-	mux.HandleFunc("/read", read)
 	mux.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", mux)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("user-data")
-	if (err != nil) {
-		uuid := uuid.New()
-		cookie = &http.Cookie{
-			Name:	"user-data",
-			Value:	uuid.String(),
-			HttpOnly: true,
-			// Secure: true,
-		}
-		http.SetCookie(w, cookie)
-		fmt.Println("added new cookie")
-	}
-	tpl.ExecuteTemplate(w, "index.gohtml", cookie)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	fmt.Println("yayaya")
 }
