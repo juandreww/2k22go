917267ac1c12c7cd9a6f195cd41b49c59835a603
juandreww <juandreww@outlook.com>
2022-09-17 19:42:54 +0700

    asdsadf

---
+++ 13x1_photoblog1_userdata/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	mux := http.DefaultServeMux
+	mux.HandleFunc("/index", index)
+	mux.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", mux)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	fmt.Println("yayaya")
+}
