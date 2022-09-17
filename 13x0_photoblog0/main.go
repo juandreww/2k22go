54b85de4a45d9fe86ecd27e7b33b8cf58dacb5bc
juandreww <juandreww@outlook.com>
2022-09-17 19:33:05 +0700

    zxcvxzcv

--- 13x0_photoblog0/main.go
+++ 13x0_photoblog0/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
-	fmt.Println("yahoo");
+	mux := http.DefaultServeMux;
+
 }
