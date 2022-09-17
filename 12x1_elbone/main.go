2c61d75143996288800200d7c7ceb4a79ac5a6b6
juandreww <juandreww@outlook.com>
2022-09-17 09:27:45 +0700

    getinstance

--- 12x1_elbone/main.go
+++ 12x1_elbone/main.go
@@ -22,6 +22,7 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/ping", ping)
 	http.HandleFunc("/instance", instance)
+	http.HandleFunc("/retrievedata", retrieveData)
     http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":80", nil)
 }
@@ -34,17 +35,37 @@ func ping(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "OK")
 }
 
-func instance(w http.ResponseWriter, req *http.Request) {
+func amigos(w http.ResponseWriter, req *http.Request) {
+	rows, err := db.Query(`SELECT aName FROM amigos;`)
+	check(err)
+
+	// data to be used in query
+	s := getInstance()
+	s += "\nRETRIEVED RECORDS:\n"
+	var name string
+
+	// query
+	for rows.Next() {
+		err = rows.Scan(&name)
+		check(err)
+		s += name + "\n"
+	}
+	fmt.Fprintln(w, s)
+}
+
+
+
+func getInstance() string {
 	resp, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err.Error()
 	}
 
 	bs := make([]byte, resp.ContentLength)
 	resp.Body.Read(bs)
 	resp.Body.Close()
-	io.WriteString(w, string(bs))
+	return string(bs)
 }
 
 func check(err error) {
