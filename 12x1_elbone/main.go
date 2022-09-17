32256b6d6bf0fec91e5cf38e45b1787e1c1c5be2
juandreww <juandreww@outlook.com>
2022-09-17 09:26:09 +0700

    cccc

--- 12x1_elbone/main.go
+++ 12x1_elbone/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"net/http"
 )
 
+var db *sql.DB
+var err error
+
 func main() {
+	db, err = sql.Open("mysql", "aws_procube1:aws_procube1@tcp(aws-procube1.chjuuutofqif.ap-southeast-3.rds.amazonaws.com:3306)/default?charset=utf8")
+    check(err)
+    defer db.Close()
+
+    err = db.Ping()
+    check(err)
+
 	http.HandleFunc("/", index)
 	http.HandleFunc("/ping", ping)
 	http.HandleFunc("/instance", instance)
@@ -33,4 +45,10 @@ func instance(w http.ResponseWriter, req *http.Request) {
 	resp.Body.Read(bs)
 	resp.Body.Close()
 	io.WriteString(w, string(bs))
+}
+
+func check(err error) {
+    if err != nil {
+        panic(err)
+    } 
 }
