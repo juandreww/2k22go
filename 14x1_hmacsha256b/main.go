1e332f67bf85edd4e1d550f1b0525edc6cb47ad0
juandreww <juandreww@outlook.com>
2022-09-18 22:10:53 +0700

    asdfasfd

--- 14x1_hmacsha256b/main.go
+++ 14x1_hmacsha256b/main.go
@@ -15,6 +15,33 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func index(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session-id")
+	if err != nil {
+		c = &http.Cookie{
+			Name:  "session-id",
+			Value: "",
+		}
+	}
+
+	if req.Method == http.MethodPost {
+		e := req.FormValue("email")
+		c.Value = e + `|` + getCode(e)
+	}
+
+	http.SetCookie(w, c)
+	io.WriteString(w, `<!DOCTYPE html>
+	<html>
+	  <body>
+	    <form method="POST">
+	      <input type="email" name="email">
+	      <input type="submit">
+	    </form>
+	    <a href="/authenticate">Validate This `+c.Value+`</a>
+	  </body>
+	</html>`)
+}
+
 func getCode(str string) string {
 	h := hmac.New(sha256.New, []byte("ourkey"))
 	io.WriteString(h, str)
