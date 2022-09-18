4dd66a8ccde082ff421597d23f7c7be7dd6c4e73
juandreww <juandreww@outlook.com>
2022-09-19 06:42:38 +0700

    asdfasdf

--- 14x7_context2/main.go
+++ 14x7_context2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,13 +21,34 @@ func index(w http.ResponseWriter, req *http.Request) {
 	ctx = context.WithValue(ctx, "userID", 777)
 	ctx = context.WithValue(ctx, "fname", "Bond")
 
-	results := dbAccess(ctx)
+	results, err := dbAccess(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusRequestTimeout)
+		return
+	}
 	fmt.Fprintln(w, results)
 }
 
-func dbAccess(ctx context.Context) int {
-	uid := ctx.Value("userID").(int)
-	return uid
+func dbAccess(ctx context.Context) (int, error) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	ch := make(chan int)
+	go func() {
+		uid := ctx.Value("userID").(int)
+		time.Sleep(10 * time.Second)
+		if ctx.Err() != nil {
+			return
+		}
+		ch <- uid
+	}()
+
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	case i := <-ch:
+		return i, nil
+	}
 }
 
 func bar(w http.ResponseWriter, req *http.Request) {
