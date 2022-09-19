2a701857ad65791b8bf632eb39ba71a3b9d3d4cd
juandreww <juandreww@outlook.com>
2022-09-19 22:41:03 +0700

    asdfasfd

--- 14x16_unmarshaljson2/main.go
+++ 14x16_unmarshaljson2/main.go
@@ -7,20 +7,28 @@ import (
 )
 
 type city struct {
-	Latitude, Longitude float64
-	City                string
+	Bali       string  `json:"Postal"`
+	Kauai      float64 `json:"Latitude"`
+	Maui       float64 `json:"Longitude"`
+	Java       string  `json:"Address"`
+	NewZealand string  `json:"City"`
+	Skye       string  `json:"State"`
+	Oahu       string  `json:"Zip"`
+	Hawaii     string  `json:"Country"`
 }
 
 type cities []city
 
 func main() {
 	var data cities
-	rcvd := `[{"precision":"zip","Latitude":37.7668,"Longitude":-122.3959,"Address":"","City":"SAN FRANCISCO","State":"CA","Zip":"94107","Country":"US"},{"precision":"zip","Latitude":37.371991,"Longitude":-122.02602,"Address":"","City":"SUNNYVALE","State":"CA","Zip":"94085","Country":"US"}]`
-	// rcvd := `{"Width":800,"Height":600,"Title":"View from 15th Floor","Thumbnail":{"Url":"http://www.example.com/image/481989943","Height":125,"Width":100},"Animated":false,"IDs":[116,943,234,38793]}`
+
+	rcvd := `[{"Postal":"zip","Latitude":37.7668,"Longitude":-122.3959,"Address":"","City":"SAN FRANCISCO","State":"CA","Zip":"94107","Country":"US"},{"Postal":"zip","Latitude":37.371991,"Longitude":-122.02602,"Address":"","City":"SUNNYVALE","State":"CA","Zip":"94085","Country":"US"}]`
+
 	err := json.Unmarshal([]byte(rcvd), &data)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	fmt.Println(data)
+	fmt.Println(data[1].Kauai)
 }
