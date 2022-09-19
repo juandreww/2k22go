b36a2952be65922f22550fa62b635f386c187379
juandreww <juandreww@outlook.com>
2022-09-19 21:34:51 +0700

    asdfasfd

--- 14x10_jsonmarshal/main.go
+++ 14x10_jsonmarshal/main.go
@@ -1,15 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
+type person struct {
+	Fname string
+	Lname string
+	Items []string
+}
+
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+	http.HandleFunc("/marshal", marshal)
+	http.HandleFunc("/encode", encode)
+	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example of server.\n"))
+	s := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>INDEX</title>
+	</head>
+	<body>
+	You are at index
+	</body>
+	</html>`
+	w.Write([]byte(s))
+}
+
+func marshal(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application-json")
+	p1 := person{
+		Fname: "Louis",
+		Lname: "Vuitton",
+		Items: []string{"Modern", "Bag", "Very original"},
+	}
+	js, err := json.Marshal(p1)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Write(js)
+}
+
+func encode(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	p1 := person{
+		Fname: "Louis",
+		Lname: "Vuitton",
+		Items: []string{"Modern", "Bag", "Very Original"},
+	}
+
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println(err)
+	}
 }
