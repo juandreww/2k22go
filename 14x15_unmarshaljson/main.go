433eaafa8478e7546576ab015ee1566318aef0e5
juandreww <juandreww@outlook.com>
2022-09-19 22:32:41 +0700

    asdf

--- 14x15_unmarshaljson/main.go
+++ 14x15_unmarshaljson/main.go
@@ -23,6 +23,7 @@ func main() {
 	var data img
 	rcvd := `{"Width":800,"Height":600,"Title":"View from 15th Floor","Thumbnail":{"Url":"http://www.example.com/image/481989943","Height":125,"Width":100},"Animated":false,"IDs":[116,943,234,38793]}`
 
+	fmt.Println(rcvd)
 	err := json.Unmarshal([]byte(rcvd), &data)
 	if err != nil {
 		log.Fatalln("error unmarshaling", err)
@@ -33,6 +34,6 @@ func main() {
 	for i, v := range data.IDs {
 		fmt.Println(i, v)
 	}
-
+	//
 	fmt.Println(data.Thumbnail.URL)
 }
