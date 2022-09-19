6fba3de28af5d7176d2935dceb4ba45a4a38c6cc
juandreww <juandreww@outlook.com>
2022-09-20 06:23:20 +0700

    asdfasdf

--- 14x18_unmarshaljson4/main.go
+++ 14x18_unmarshaljson4/main.go
@@ -6,21 +6,12 @@ import (
 	"log"
 )
 
-type city struct {
-	Latitude, Longitude float64
-	City                string
-}
-
-type cities []city
-
 func main() {
-	var data cities
-	rcvd := `[{"precision":"zip","Latitude":37.7668,"Longitude":-122.3959,"Address":"","City":"SAN FRANCISCO","State":"CA","Zip":"94107","Country":"US"},{"precision":"zip","Latitude":37.371991,"Longitude":-122.02602,"Address":"","City":"SUNNYVALE","State":"CA","Zip":"94085","Country":"US"}]`
-	// rcvd := `{"Width":800,"Height":600,"Title":"View from 15th Floor","Thumbnail":{"Url":"http://www.example.com/image/481989943","Height":125,"Width":100},"Animated":false,"IDs":[116,943,234,38793]}`
+	var data string
+	rcvd := `"Alice in Borderland"`
 	err := json.Unmarshal([]byte(rcvd), &data)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	fmt.Println(data)
 }
