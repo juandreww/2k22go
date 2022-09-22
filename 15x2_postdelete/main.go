d6bdf5bc78b031bb9a9c649e1c050e150e4ed541
juandreww <juandreww@outlook.com>
2022-09-22 22:32:03 +0700

    ASDFSADF

---
+++ 15x2_postdelete/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/juandreww/2k22go/15x2_postdelete/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	rt := httprouter.New()
+	rt.GET("/index", index)
+	rt.GET("/user/:id", getUser)
+	rt.POST("/user", createUser)
+	rt.DELETE("/user/:id", deleteUser)
+	http.ListenAndServe("localhost:8080", rt)
+}
+
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	str := `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		<meta charset="UTF-8">
+		<title>Index</title>
+		</head>
+		<body>
+		<a href="/user/9872309847">GO TO: http://localhost:8080/user/9872309847</a>
+		</body>
+		</html>
+	`
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(str))
+}
+
+func getUser(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "Avaleska Zhang",
+		Gender: "female",
+		Age:    18,
+		Id:     p.ByName("id"),
+	}
+
+	js, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", js)
+}
