09e3fd50f7f48ebe836d80fe315bdfd95452ab44
juandreww <juandreww@outlook.com>
2022-09-23 21:08:31 +0700

    sss

--- 15x5_mongodb1/main.go
+++ 15x5_mongodb1/main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"net/http"
 
-	"github.com/juandreww/2k22go/15x4_controllers/controllers"
+	"github.com/juandreww/2k22go/15x5_mongodb1/controllers"
 	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 )
 
 func main() {
 	rt := httprouter.New()
-	uc := controllers.NewUserController()
+	uc := controllers.NewUserController(getSession())
 	rt.GET("/user/:id", uc.GetUser)
 	rt.POST("/user", uc.CreateUser)
 	rt.DELETE("/user/:id", uc.DeleteUser)
 	http.ListenAndServe("localhost:8080", rt)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return s
 }
