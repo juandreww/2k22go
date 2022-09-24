cc912e521378960afee705ced80a36dce5c6431f
juandreww <juandreww@outlook.com>
2022-09-24 14:15:21 +0700

    asdf

--- 15x7_withoutmongodb/controllers/user.go
+++ 15x7_withoutmongodb/controllers/user.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,43 +10,26 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var ctx = context.Background()
-
-var maps = make(map[models.Contacts]models.Contacts)
+// var ctx = context.Background()
 
 type UserController struct {
+	session map[string]models.Contacts
 }
 
-func NewUserController() *UserController {
-	return &UserController{}
+func NewUserController(mp map[string]models.Contacts) *UserController {
+	return &UserController{mp}
 }
 
 func (uc UserController) GetUser(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
-	maps[models.Contacts{"Fname", "Lname"}] = models.Contacts{"Abigail", "Ayu"}
-	fmt.Println(maps)
+	fname := p.ByName("fname")
+	ct := uc.session[fname]
 
-	// data, err := uc.cl.Collection("contacts").Find(ctx, bson.M{"Fname": p.ByName("fname")})
-	// checkError(err)
-	// defer data.Close(ctx)
+	ctm, err := json.Marshal(ct)
+	checkError(err)
 
-	// result := make([]models.Contacts, 0)
-	// for data.Next(ctx) {
-	// 	var row models.Contacts
-	// 	err := data.Decode(&row)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-
-	// 	result = append(result, row)
-	// }
-
-	// if len(result) > 0 {
-	// 	for _, v := range result {
-	// 		fmt.Println(v.Fname, v.Lname)
-	// 	}
-	// } else {
-	// 	fmt.Println("Not Found with Contact Name: " + p.ByName("fname"))
-	// }
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK) // 200
+	fmt.Fprintf(w, "%s\n", ctm)
 }
 
 // func (uc UserController) CreateUser(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
--- 15x7_withoutmongodb/main.go
+++ 15x7_withoutmongodb/main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/juandreww/2k22go/15x7_withoutmongodb/controllers"
+	"github.com/juandreww/2k22go/15x6_mongodb2/controllers"
+	"github.com/juandreww/2k22go/15x7_withoutmongodb/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -13,12 +14,17 @@ var ctx = context.Background()
 
 func main() {
 	rt := httprouter.New()
+	db := clientSession()
 
 	fmt.Println("You are connected to MongoDB")
-	uc := controllers.NewUserController()
+	uc := controllers.NewUserController(db)
 	rt.GET("/user/:fname", uc.GetUser)
-	// rt.POST("/user", uc.CreateUser)
-	// rt.POST("/userupdate/:fname", uc.UpdateUser)
-	// rt.DELETE("/user/:fname", uc.DeleteUser)
+	rt.POST("/user", uc.CreateUser)
+	rt.POST("/userupdate/:fname", uc.UpdateUser)
+	rt.DELETE("/user/:fname", uc.DeleteUser)
 	http.ListenAndServe("localhost:8080", rt)
+}
+
+func clientSession() map[string]models.Contacts {
+	return make(map[string]models.Contacts)
 }
