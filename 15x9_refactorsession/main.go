54d675d742cf7fa01c3b1875bb13569f0c9481fb
juandreww <juandreww@outlook.com>
2022-09-24 15:34:32 +0700

    asdfasdf

---
+++ 15x9_refactorsession/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/juandreww/2k22go/15x8_insertintofile/controllers"
+	"github.com/juandreww/2k22go/15x8_insertintofile/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+// var ctx = context.Background()
+
+func main() {
+	rt := httprouter.New()
+
+	fmt.Println("You are connected to MongoDB")
+	uc := controllers.NewUserController(clientSession())
+	rt.GET("/user/:fname", uc.GetUser)
+	rt.POST("/user", uc.CreateUser)
+	rt.DELETE("/user/:fname", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", rt)
+}
+
+func clientSession() map[string]models.Contacts {
+	return make(map[string]models.Contacts)
+}
