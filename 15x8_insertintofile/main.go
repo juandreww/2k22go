d222325cac904e1d74273ed4d13e98563e52fb45
juandreww <juandreww@outlook.com>
2022-09-24 14:51:48 +0700

    asdfasf

--- 15x8_insertintofile/main.go
+++ 15x8_insertintofile/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
@@ -10,7 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var ctx = context.Background()
+// var ctx = context.Background()
 
 func main() {
 	rt := httprouter.New()
