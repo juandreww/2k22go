503b56ad76d4517f7f2673774814f7f1fcee964f
juandreww <juandreww@outlook.com>
2022-09-26 05:48:37 +0700

    asdfasdf

--- 16x2_docker2/main.go
+++ 16x2_docker2/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT env is required")
+	}
+
+	instanceID := os.Getenv("INSTANCE_ID")
+
+	http.HandleFunc("/", index)
 	http.HandleFunc("/index1", index)
 	http.HandleFunc("/index2", index2)
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, _ *http.Request) {
