72aec7e140ac49844f099ebe3d5df857acbbd58a
juandreww <juandreww@outlook.com>
2022-09-28 06:11:28 +0700

    asdf

---
+++ 17x1_pgsqlgo1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://clara:password@localhost/cube0?sslmode=disable")
+	checkError(err)
+	defer db.Close()
+
+	err = db.Ping()
+	checkError(err)
+
+	fmt.Println("Welcome to the postgres.")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
