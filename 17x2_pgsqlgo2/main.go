fcf6dd874aff99c9fea45ddd8b1147fce3d78a1d
juandreww <juandreww@outlook.com>
2022-09-28 06:36:21 +0700

    asdfasdf

---
+++ 17x2_pgsqlgo2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Pricing struct {
+	ID    string
+	Title string
+	Price float32
+}
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://clara:password@localhost/cube0?sslmode=disable")
+	checkError(err)
+	defer db.Close()
+
+	err = db.Ping()
+	checkError(err)
+
+	fmt.Println("Welcome to the postgres.")
+	rows, err := db.Query("SELECT * FROM pricing;")
+	checkError(err)
+	defer rows.Close()
+
+	prices := make([]Pricing, 0)
+	for rows.Next() {
+		pc := Pricing{}
+		err := rows.Scan(&pc.ID, &pc.Title, &pc.Price)
+		checkError(err)
+		prices = append(prices, pc)
+	}
+
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	for _, pc := range prices {
+		fmt.Printf("%s, %s, %.2f\n", pc.ID, pc.Title, pc.Price)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
