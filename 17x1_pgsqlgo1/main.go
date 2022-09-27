31107c820d6ac2697ff14f541cf4b0cd56e18634
juandreww <juandreww@outlook.com>
2022-09-28 06:30:50 +0700

    asdasdf

--- 17x1_pgsqlgo1/main.go
+++ 17x1_pgsqlgo1/main.go
@@ -22,7 +22,20 @@ func main() {
 	checkError(err)
 
 	fmt.Println("Welcome to the postgres.")
+	rows, err := db.Query("SELECT * FROM pricing;")
+	checkError(err)
+	defer rows.Close()
 
+	prices := make([]Pricing, 0)
+	for rows.Next() {
+		pc := Pricing{}
+		err := rows.Scan(&pc.ID, &pc.Title, &pc.Price)
+		checkError(err)
+		prices = append(prices, pc)
+	}
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
 }
 
 func checkError(err error) {
