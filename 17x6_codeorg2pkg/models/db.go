d4f266318720ecdb0d12797d38be91f89d8e9161
juandreww <juandreww@outlook.com>
2022-09-28 22:23:44 +0700

    asdf

--- 17x6_codeorg2pkg/models/db.go
+++ 17x6_codeorg2pkg/models/db.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("postgres", "postgres://clara:password@localhost/cube0?sslmode=disable")
+	checkError(err)
+
+	err = db.Ping()
+	checkError(err)
+
+	fmt.Println("Welcome to the postgres.")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- 17x6_codeorg2pkg/models/prices.go
+++ 17x6_codeorg2pkg/models/prices.go
@@ -1,1 +1,29 @@
 package models
+
+import (
+	"errors"
+	"net/http"
+)
+
+type Pricing struct {
+	ID    string
+	Title string
+	Price float32
+}
+
+func OnePrice(r *http.Request) (Pricing, error) {
+	pc := Pricing{}
+	id := r.FormValue("id")
+	if id == "" {
+		return pc, errors.New("400. Bad Request")
+	}
+
+	row := db.QueryRow("SELECT * FROM pricing WHERE id = $1;", id)
+	err := row.Scan(&pc.ID, &pc.Title, &pc.Price)
+
+	if err != nil {
+		return pc, err
+	}
+
+	return pc, nil
+}
