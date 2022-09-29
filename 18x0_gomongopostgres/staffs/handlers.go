989ef4f626792a0ce109b4117497ee1e1f3f2c28
juandreww <juandreww@outlook.com>
2022-09-30 06:18:05 +0700

    asdf

--- 18x0_gomongopostgres/staffs/handlers.go
+++ 18x0_gomongopostgres/staffs/handlers.go
@@ -3,5 +3,16 @@ package staffs
 import "net/http"
 
 func List(w http.Response, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
 
+	list, err := AllStaffs()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	//
 }
