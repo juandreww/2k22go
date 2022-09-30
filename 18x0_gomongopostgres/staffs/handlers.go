7cfd38d96ed863d2f77bc6f729ac9de44db4d98e
juandreww <juandreww@outlook.com>
2022-09-30 07:20:12 +0700

    asdfasdf

--- 18x0_gomongopostgres/staffs/handlers.go
+++ 18x0_gomongopostgres/staffs/handlers.go
@@ -1,22 +1,26 @@
 package staffs
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/juandreww/2k22go/18x0_gomongopostgres/config"
 )
 
 func List(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Here?")
 	if r.Method != "GET" {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
 
-	list, err := AllStaffs()
-	if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
+	// list, err := AllStaffs()
+	// if err != nil {
+	// 	http.Error(w, http.StatusText(500), 500)
+	// 	return
+	// }
+
+	list := 0
 
 	config.TPL.ExecuteTemplate(w, "staffs.gohtml", list)
 }
