7c4643542c4675c52fb4565db1b63b389cd0d0d3
juandreww <juandreww@outlook.com>
2022-09-30 07:00:51 +0700

    handlers staff

--- 18x0_gomongopostgres/staffs/models.go
+++ 18x0_gomongopostgres/staffs/models.go
@@ -1,1 +1,36 @@
 package staffs
+
+import "github.com/juandreww/2k22go/18x0_gomongopostgres/config"
+
+type Staff struct {
+	ID       string
+	Name     string
+	UserName string
+	Password string
+	IsActive bool
+}
+
+func AllStaffs() ([]Staff, error) {
+	rows, err := config.DB.Query("SELECT * FROM employees;")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	list := make([]Staff, 0)
+	for rows.Next() {
+		p := Staff{}
+		err := rows.Scan(&p.ID, &p.Name, &p.UserName, &p.Password, &p.IsActive)
+		checkError(err)
+
+		list = append(list, p)
+	}
+
+	return list, nil
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
