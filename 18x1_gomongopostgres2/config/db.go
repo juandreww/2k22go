eafb5da8c78d02f68a222c014242e1b3134a9414
juandreww <juandreww@outlook.com>
2022-10-01 16:02:28 +0700

    mongodb

--- 18x1_gomongopostgres2/config/db.go
+++ 18x1_gomongopostgres2/config/db.go
@@ -1,23 +1,28 @@
 package config
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 
 	_ "github.com/lib/pq"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var DB *sql.DB
+var DB *mongo.Database
+var ctx = context.Background()
 
 func init() {
-	var err error
-	DB, err = sql.Open("postgres", "postgres://clara:password@localhost/cube0?sslmode=disable")
+	cOpt := options.Client().ApplyURI("mongodb://admin:123@localhost:27017")
+
+	cl, err := mongo.NewClient(cOpt)
 	checkError(err)
 
-	err = DB.Ping()
+	err = cl.Connect(ctx)
 	checkError(err)
 
-	fmt.Println("Welcome to the postgres.")
+	fmt.Println("Welcome to the mongodb.")
+	DB = cl.Database("dbtest")
 }
 
 func checkError(err error) {
--- 18x1_gomongopostgres2/main.go
+++ 18x1_gomongopostgres2/main.go
@@ -4,20 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/juandreww/2k22go/18x1_gomongopostgres2/prices"
 	"github.com/juandreww/2k22go/18x1_gomongopostgres2/staffs"
 )
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/index", prices.Index)
-	http.HandleFunc("/index/show", prices.IndexShow)
-	http.HandleFunc("/index/create", prices.IndexCreateForm)
-	http.HandleFunc("/index/create/process", prices.IndexCreateProcess)
-	http.HandleFunc("/index/update", prices.IndexUpdateForm)
-	http.HandleFunc("/index/update/process", prices.IndexUpdateProcess)
-	http.HandleFunc("/index/delete/process", prices.IndexDeleteProcess)
-
 	http.HandleFunc("/staff", staffs.List)
 	http.HandleFunc("/staff/new/form", staffs.NewStaffForm)
 	http.HandleFunc("/staff/new/save", staffs.NewStaffSave)
